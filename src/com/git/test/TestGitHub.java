187dbff6eceeac89b531e68c418d2306b27a55bb
AlanCZ <1054717708@qq.com>
2017-08-17 21:10:17 +0800

    TestGitHub_01

---
+++ src/com/git/test/TestGitHub.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+public class TestGitHub {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 30; i++) {
+			System.out.println("循环第： " + i + "次！");
+		}
+
+	}
+
+}
