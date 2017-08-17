0fda94fdb781361cb427b1cfcde470c5be0683bf
AlanCZ <1054717708@qq.com>
2017-08-17 23:19:14 +0800

    new a index.jsp

--- src/com/git/test/TestGitHub.java
+++ src/com/git/test/TestGitHub.java
@@ -6,7 +6,7 @@ public class TestGitHub {
 
 		for (int i = 0; i < 30; i++) {
 			System.out.println("循环第： " + i + "次！");
-                        System.out.println("啦啦啦qweqwe");
+			System.out.println("啦啦啦qweqwe");
 		}
 
 	}
